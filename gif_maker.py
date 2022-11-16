ff1c99e5cd92059e429c43beaee719aa6b6c0bd9
Jacob Ney <jacobney@colostate.edu>
2022-11-16 15:19:40 -0700

    Lit gif
    Adds the script to create a gif from pngs (of Lit)

--- gif_maker.py
+++ gif_maker.py
@@ -1,16 +1,14 @@
 #Makes gifs for fire spread
 
 #Input Image path:
-#C:\\Users\\16516\\Desktop\\qf_visualizations\\line_ign_headfire
+#C:\Users\neyja\OneDrive\Documents\GitHub\qf_visualizations\LIT
 
 reticulate::repl_python(
   
   python3 -m pip install Pillow
 
-import glob
-from PIL import Image
 def make_gif(frame_folder):
-    frames = [Image.open(image) for image in glob.glob(f"{C:/Users/16516/Desktop/qf_visualizations/line_ign_headfire}/*.PNG")]
+    frames = [Image.open(image) for image in glob.glob(f"{C:\Users\neyja\OneDrive\Documents\GitHub\qf_visualizations\LIT}/*.PNG")]
     frame_one = frames[0]
     frame_one.save("QuicFire.gif", format="GIF", append_images=frames,
                save_all=True, duration=35, loop=0)
@@ -19,6 +17,32 @@ if __name__ == "__main__":
     make_gif("C:/Users/16516/Desktop/qf_visualizations/line_ign_headfire/output?")
     
     import glob
+##################ANOTHER WAY####################
+
+
+import os
+
+path = os.getcwd()
+
+print(path)
+
+getcwd()
+
+from PIL import Image
+import glob
+
+# Create the frames
+frames = []
+imgs = glob.glob("C:\\Users\\neyja\\OneDrive\\Documents\\GitHub\\qf_visualizations\\LIT\\*.png")
+for i in imgs:
+    new_frame = Image.open(i)
+    frames.append(new_frame)
+
+# Save into a GIF file that loops forever
+frames[0].save('C:\\Users\\neyja\\OneDrive\\Documents\\GitHub\\qf_visualizations\\LIT\\png_to_gif.gif', format='GIF',
+               append_images=frames[1:],
+               save_all=True,
+               duration=300, loop=0)
 
 #OG Copy
 
--- gif_script.R
+++ gif_script.R
@@ -1,30 +1,23 @@
 #Makes a gif for qf spread
 
-install.packages("reticulate")
-library(reticulate)
-
-reticulate::repl_python(
-  
-)
-
-#C:\\Users\\16516\\Desktop\\qf_visualizations\\line_ign_headfire
-
-dir_out <- file.path(tempdir(), "tx-sales")
-dir.create(dir_out, recursive = TRUE)
+library(magick)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
 
 ## list file names and read in
-imgs <- list.files(C:\\Users\\16516\\Desktop\\qf_visualizations\\line_ign_headfire, full.names = TRUE)
+imgs <- list.files("C:\\Users\\neyja\\OneDrive\\Documents\\GitHub\\qf_visualizations\\LIT", full.names = TRUE)
 img_list <- lapply(imgs, image_read)
 
 ## join the images together
 img_joined <- image_join(img_list)
 
 ## animate at 2 frames per second
-img_animated <- image_animate(img_joined, fps = 0.5)
+img_animated <- image_animate(img_joined, fps = 2)
 
 ## view animated image
 img_animated
 
 ## save to disk
 image_write(image = img_animated,
-            path = "C:\\Users\\16516\\Desktop\\qf_visualizations\\line_ign_headfire\\gif")
+            path = "C:\\Users\\neyja\\OneDrive\\Documents\\GitHub\\qf_visualizations\\LIT.gif")
